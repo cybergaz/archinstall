fbbf21200590cf7ac7c7d340c2b62233725950f9
cybergaz <kkanttechy@gmail.com>
2024-01-08 01:15:00 +0530

    updated with iwd removed networkmanager

--- archinstall.sh
+++ archinstall.sh
@@ -61,9 +61,11 @@ sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
 sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -Sy --noconfirm networkmanager wpa_supplicant rsync zip unzip unrar fzf git vim ttf-jetbrains-mono
+pacman -Sy --noconfirm iwd rsync zip unzip unrar git gvim ttf-jetbrains-mono
 
-systemctl enable NetworkManager.service
+mkdir /etc/iwd
+echo -e "[General]\nEnableNetworkConfiguration=true\n\n[Network]\nEnableIPv6=true" >/etc/iwd/main.conf
+systemctl enable systemd-networkd systemd-resolved iwd
 
 echo "Enter Username: "
 read username
